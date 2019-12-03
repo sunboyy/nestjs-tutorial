2cbab7568d5f0696720ff72805121b3bcca69cc7
sunboyy <sura_sun@hotmail.com>
2019-12-03 16:10:27 +0700

    Add ValidationPipe and ParseIntPipe

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UsePipes, Param } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { Cat } from './interfaces/cat.interface';
+import { ValidationPipe } from 'src/common/pipes/validation.pipe';
+import { ParseIntPipe } from 'src/common/pipes/parse-int.pipe';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Post()
+  @UsePipes(ValidationPipe)
   create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
@@ -15,5 +18,10 @@ export class CatsController {
   @Get()
   async findAll(): Promise<Cat[]> {
     return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id) {
+    return this.catsService.findOne(id);
   }
 }
